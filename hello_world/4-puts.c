72d01b59ca64115f05a8be917e05ea2a0dff460d
RaboAnas <anasrabo335@gmail.com>
2024-10-21 10:23:35 +0000

    0-preprocessor

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,1 +1,11 @@
-gcc -o 4-puts_0 -Wall -Werror -Wextra -pedantic -std=gnu89 4-puts.c
+#include <stdio.h>
+/**
+ * main - Prints out output using puts
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+        puts("\"Programming is like building a multilingual puzzle");
+        return (0);
+}
