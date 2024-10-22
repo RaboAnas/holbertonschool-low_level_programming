6d9a5e7f804c8f3fc11fc855e66e01faabe1031d
AnasRabo <anasrabo335@gmail.com>
2024-10-22 13:03:24 +0000

    varriables

--- variables_if_else_while/5-print_numbers.c
+++ variables_if_else_while/5-print_numbers.c
@@ -2,20 +2,17 @@
 /**
  * main - Entry point
  *
- * Description: Prints the alphabet in lowercase except for 'q' and 'e',
+ * Description: Prints all single digit numbers of base 10 starting from 0,
  *              followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-char ch;
-for (ch = 'a'; ch <= 'z'; ch++)
+int i;
+for (i = 0; i < 10; i++)
 {
-if (ch != 'q' && ch != 'e')
-{
-putchar(ch);
-}
+putchar(i + '0');
 }
 putchar('\n');
 return (0);
