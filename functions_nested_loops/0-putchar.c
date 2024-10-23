4321bcb3ef83455276f5836b41bcaaea1a3d4fc8
RaboAnas <anasrabo335@gmail.com>
2024-10-23 09:48:45 +0000

    6-print_numberz.c

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,14 +1,27 @@
-#include <stdio.h>
+#include "main.h"
+#include <unistd.h>  // For the write function
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c) {
+    return write(1, &c, 1);  // Write the character to stdout
+}
 
 int main(void) {
-  putchar('_');
-    putchar('p');
-    putchar('u');
-    putchar('t');
-    putchar('c');
-    putchar('h');
-    putchar('a');
-    putchar('r');
-    putchar('\n');
-    return 0;
+    _putchar('_');  // Print '_'
+    _putchar('p');  // Print 'p'
+    _putchar('u');  // Print 'u'
+    _putchar('t');  // Print 't'
+    _putchar('c');  // Print 'c'
+    _putchar('h');  // Print 'h'
+    _putchar('a');  // Print 'a'
+    _putchar('r');  // Print 'r'
+    _putchar('\n'); // Print a new line
+
+    return 0; // Indicate successful termination
 }
