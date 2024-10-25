1d35ed1f367a3fa94a33885a094787e5939cae6a
RaboAnas <anasrabo335@gmail.com>
2024-10-25 14:06:22 +0000

    2

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,14 +1,20 @@
-#include <unistd.h>
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{ echo {a..z}
+*print_alphabet - Entry point 
+*
+* Return: Always 0 (success)
+*/
 
-	return (write(1, &c, 1));
+void print_alphabet(void)
+{
+  char alpha = 'a';
+
+  while (alpha <= 'z')
+    {
+      _putchar(alpha);
+      alpha++;
+    }
+  _putchar('\n');
+
 }
