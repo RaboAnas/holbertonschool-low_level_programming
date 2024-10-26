8633fec6d2d536de42af8b784b45cbbe2bd3bd42
RaboAnas <anasrabo335@gmail.com>
2024-10-26 12:45:51 +0000

    3

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -13,19 +13,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+'); /* Print '+' for positive numbers */
-		_putchar('\n'); /* Print newline for formatting */
 		return (1); /* Return 1 for positive numbers */
 	}
 	else if (n == 0)
 	{
 		_putchar('0'); /* Print '0' for zero */
-		_putchar('\n'); /* Print newline for formatting */
 		return (0); /* Return 0 for zero */
 	}
 	else
 	{
 		_putchar('-'); /* Print '-' for negative numbers */
-		_putchar('\n'); /* Print newline for formatting */
 		return (-1); /* Return -1 for negative numbers */
 	}
 }
+
