2c4ca2a8ea84e9e610ed06fb1121bbae7a65a314
RaboAnas <anasrabo335@gmail.com>
2024-10-26 09:49:10 +0000

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,20 +1,21 @@
-#inclunde "main.h"
-
+#include "main.h"
+/**
+ * print_alphabet_x10 - Fonction qui affiche l'alphabet en minuscules 10 fois
+ * Chaque alphabet est suivi d'un saut de ligne
+ */
 void print_alphabet_x10(void)
 
 {
 	int alphabet10;
 	int a;
-	for (a = 1; a <= 10; a ++)
-	{
-		for (alphabet10 = 'a' ; alphabet10 <= 'z' ; alphabet10++)
-	}
+for (a = 1; a <= 10; a++)
+{
+for (alphabet10 = 'a' ; alphabet10 <= 'z' ; alphabet10++)
+
 		{
 	_putchar(alphabet10);
 		}
 
 _putchar('\n');
-
 }
-
-
+}
