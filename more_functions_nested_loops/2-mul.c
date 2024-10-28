6280dad607430cc44b18a1239eda6478385cab97
RaboAnas <anasrabo335@gmail.com>
2024-10-28 14:16:24 +0000

    0

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - Multiplie deux nombres entiers.
+* @a: le premier nombre entier.
+* @b: le deuxième nombre entier.
+*
+* Return: le produit de a et b.
+*/
+
+int mul(int a, int b)
+
+{
+int zoro = a * b;
+return (zoro);
+}
