f6237156df90cf9f89091ad330ec756af56de571
RaboAnas <anasrabo335@gmail.com>
2024-10-29 15:00:23 +0000

    5

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,7 @@
 #include "main.h"
-
 /**
- * more_numbers - Imprime les nombres de 0 à 14, dix fois.
- */
+* more_numbers - Prints the numbers from 0 to 14, 10 times.
+*/
 void more_numbers(void)
 {
 int i, j;
@@ -11,14 +10,9 @@ for (i = 0; i < 10; i++)
 for (j = 0; j <= 14; j++)
 {
 if (j > 9)
-{
-_putchar('1');
-_putchar(j % 10 + '0');
-}
+_putchar('1'), _putchar(j % 10 + '0');
 else
-{
 _putchar(j + '0');
-}
 }
 _putchar('\n');
 }
